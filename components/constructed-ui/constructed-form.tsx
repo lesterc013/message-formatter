4c7797ae91be5136a40cb35f7d4b4ac789cfde99
lester-mmta-pc <lesterc013@gmail.com>
2025-05-10 15:39:27 +0800

    Updated UI to show submitting state and copy to clipboard function

--- components/constructed-ui/constructed-form.tsx
+++ components/constructed-ui/constructed-form.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+/// Forms
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+/// Hooks
+import { useState } from "react";
+import { useCopyToClipboard } from "usehooks-ts";
+
+/// UI
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -14,8 +20,9 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
 import { Label } from "../ui/label";
+import { SyncLoader } from "react-spinners";
+import { Copy } from "lucide-react";
 
 // For now, the form just has one field which is the input for the whatsapp msg that must be a string
 const formSchema = z.object({
@@ -26,10 +33,13 @@ export type MyFormData = z.infer<typeof formSchema>;
 
 export default function ConstructedForm() {
   const [parsedMessage, setParsedMessage] = useState("");
+  const [copiedMessage, copy] = useCopyToClipboard();
+
   // Configure all the form requirements first like RHF, zod etc
   const form = useForm<MyFormData>({
     resolver: zodResolver(formSchema),
   });
+  const formSubmitting = form.formState.isSubmitting;
 
   // API routes are always accessed from the root URL at /api/[folder-name]
   const onSubmit: SubmitHandler<MyFormData> = async (
@@ -38,25 +48,39 @@ export default function ConstructedForm() {
     // As of now, the formSubmission is only {input: "..."}
     // If i JSON.stringify, then ill get {"input": "..."} which isnt what openAI API wants right
     const body = JSON.stringify(formSubmission);
-    const response = await fetch("/api/format-message", {
-      method: "POST",
-      body: body,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    try {
+      const response = await fetch("/api/format-message", {
+        method: "POST",
+        body: body,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    // Unjson the response
-    const parsed = await response.json();
+      // Unjson the response
+      const parsed = await response.json();
 
-    // Set the state to update the UI
-    setParsedMessage(parsed);
+      // Set the state to update the UI
+      setParsedMessage(parsed);
+    } catch (error) {
+      console.log("Error in onSubmit form: " + error);
+    }
+  };
+
+  const handleCopy = (message: string) => {
+    copy(message)
+      .then(() => {
+        console.log("Copied!", { message });
+      })
+      .catch((error) => {
+        console.error("Failed to copy!", error);
+      });
   };
 
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
             control={form.control}
             name="input"
@@ -70,11 +94,26 @@ export default function ConstructedForm() {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit" disabled={formSubmitting ? true : false}>
+            Submit
+          </Button>
         </form>
       </Form>
+
       <Label>Output</Label>
-      <Textarea value={parsedMessage} readOnly />
+      {formSubmitting ? (
+        <SyncLoader size={10} />
+      ) : (
+        <div>
+          <Textarea
+            value={parsedMessage}
+            onChange={(e) => setParsedMessage(e.target.value)}
+          />
+          <Button onClick={() => handleCopy(parsedMessage)}>
+            <Copy />
+          </Button>
+        </div>
+      )}
     </>
   );
 }
