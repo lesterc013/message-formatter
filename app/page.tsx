00cfa97821aa1574c9378d28f9436ac1d4ec9a5f
lester-mmta-pc <lesterc013@gmail.com>
2025-05-10 15:39:49 +0800

    Removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import ConstructedForm from "@/components/constructed-ui/constructed-form";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
