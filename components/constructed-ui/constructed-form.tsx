edee9544755e847feb1beada722ede19986870f2
lester-mmta-pc <lesterc013@gmail.com>
2025-05-10 16:24:23 +0800

    Edit to allow auto textarea 2

--- components/constructed-ui/constructed-form.tsx
+++ components/constructed-ui/constructed-form.tsx
@@ -90,12 +90,13 @@ export default function ConstructedForm() {
               <FormItem>
                 <FormLabel>Paste WhatsApp Message Here</FormLabel>
                 <FormControl>
-                  {/* <Textarea {...field} /> */}
-                  <TextareaAutosize
-                    minRows={5}
-                    className={cn(
-                      "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-                    )}
+                  <Textarea
+                    {...field}
+                    onInput={(e) => {
+                      const target = e.target as HTMLTextAreaElement;
+                      target.style.height = "0px";
+                      target.style.height = target.scrollHeight + "px";
+                    }}
                   />
                 </FormControl>
                 <FormMessage />
