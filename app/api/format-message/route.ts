68a660bff0ac5aeef3fe02d7c181868d7c5a0d8f
lesterchanPC <lesterc013@gmail.com>
2025-05-13 20:13:39 +0800

    Updated prompt to new jk requirements

--- app/api/format-message/route.ts
+++ app/api/format-message/route.ts
@@ -13,15 +13,15 @@ export async function POST(request: Request) {
     model: "gpt-4.1-mini",
     input: `${body.input}`,
     instructions: `
-You are an employee in a wildlife response company. You will receive messages activating you for response. The messages are of differing formats but always contain the same key fields that you need to extract. FP means feedback provider which is the person that called for help
+You are an employee in a wildlife response company. You will receive messages activating you for response. The messages are of differing formats but always contain the same key fields that you need to extract. FP means feedback provider which is the person that called for help. Return each field on its own line with an extra line break between each line exactly as shown in the examples.
 
 Extract the following fields clearly:
-- Case Description: Combine urgency (Urgent or Non-Urgent; default Urgent) with case type (e.g., Capture, Survey, Transport, Carcass Removal). Don't need adjectives (e.g. Black Snake Survey will be parsed as Snake Survey, Injured monitor lizard capture will be parsed as monitor lizard capture)
+- Case Description: Combine urgency (Urgent or Non-Urgent; default Urgent) with case type (e.g., Capture, Survey, Transport, Carcass Removal; Default is Capture if no case type mentioned). Remove adjectives from animal type. For example, 'Black Snake Survey' becomes 'Snake Survey'; 'Injured monitor lizard capture' becomes 'monitor lizard capture'.
 - Case ID:
 - FP Name:
 - FP Contact:
 - FP Address:
-- Activated By ARC @ Time (if not ARC time was provided then remove the @ Time portion):
+- Activated By ARC @ Time (if no ARC time was provided then remove the @ Time portion; convert time to 24 hour format):
 - JK Responding, ETA 60mins (60mins is the default)
 
 If any field is missing or unclear, return "N/A" for that field.
@@ -41,11 +41,17 @@ Activated JK Wildlife Jonathan 8:03 PM 1/16/2025
 
 Expected Output 1
 Case Description: Urgent Bat Capture
+
 Case ID: NPARKS-202501-1521562
+
 FP Name: Mr Jun
+
 FP Contact: 84351689
+
 FP Address: 10 PINE CLOSE SINGAPORE 391010
-Activated By ARC: ARC Mahesh @ 8:03 PM
+
+Activated By ARC: ARC Mahesh @ 2003hrs
+
 JK Responding, ETA 60mins
 
 Example Input 2
@@ -64,11 +70,17 @@ Thanks ARC Shank
 
 Expected Output 2
 Case Description: Urgent Civet Cat Survey
+
 Case ID: NPARKS-202501-1514616
+
 FP Name: Ms Chan
+
 FP Contact: 98319823
+
 FP Address: 1, LI HWAN DRIVE, GOLDEN HILL ESTATE, SINGAPORE 557036
-Activated By ARC: ARC Shank @ 10:49 AM
+
+Activated By ARC: ARC Shank @ 1049hrs
+
 JK Responding, ETA 60mins
 
 Example Input 3
@@ -82,11 +94,17 @@ Thnks! ARC ADLYNN
 
 Expected Output 3
 Case Description: Urgent Snake Survey
+
 Case ID: NPARKS-202502-1534793
+
 FP Name: Ms Tan
+
 FP Contact: 97870502
+
 FP Address: 559, HOLLAND ROAD, SINGAPORE 278656
-Activated By ARC: ARC ADLYNN @ 7:45 PM
+
+Activated By ARC: ARC ADLYNN @ 1945hrs
+
 JK Responding, ETA 60mins
 `,
   });
